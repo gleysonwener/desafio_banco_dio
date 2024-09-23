faf85e1c1fddb6e07bb11bee3dfdcdaf9dbad000
gleyson <wenerpython@gmail.com>
2024-09-23 13:55:43 -0300

    desafio2

--- desafio2.py
+++ desafio2.py
@@ -2,7 +2,7 @@ import textwrap
 
 def menu():
     menu = """\n
-    ================ MENU ================
+    = MENU =
     [d]\tDepositar
     [s]\tSacar
     [e]\tExtrato
@@ -18,9 +18,9 @@ def depositar(saldo, valor, extrato, /):
     if valor > 0:
         saldo += valor
         extrato += f"Depósito:\tR$ {valor:.2f}\n"
-        print("\n=== Depósito realizado com sucesso! ===")
+        print("\n Depósito realizado com sucesso! ")
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n Operação falhou! O valor informado é inválido. ")
 
     return saldo, extrato
 
@@ -31,31 +31,31 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saques = numero_saques >= limite_saques
 
     if excedeu_saldo:
-        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+        print("\n Operação falhou! Você não tem saldo suficiente. ")
 
     elif excedeu_limite:
-        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        print("\n Operação falhou! O valor do saque excede o limite. ")
 
     elif excedeu_saques:
-        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+        print("\n Operação falhou! Número máximo de saques excedido. ")
 
     elif valor > 0:
         saldo -= valor
         extrato += f"Saque:\t\tR$ {valor:.2f}\n"
         numero_saques += 1
-        print("\n=== Saque realizado com sucesso! ===")
+        print("\n Saque realizado com sucesso! ")
 
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n Operação falhou! O valor informado é inválido.")
 
     return saldo, extrato
 
 
 def exibir_extrato(saldo, /, *, extrato):
-    print("\n================ EXTRATO ================")
+    print("\n= EXTRATO =")
     print("Não foram realizadas movimentações." if not extrato else extrato)
     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
-    print("==========================================")
+    print("")
 
 
 def criar_usuario(usuarios):
@@ -63,7 +63,7 @@ def criar_usuario(usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        print("\n Já existe usuário com esse CPF! ")
         return
 
     nome = input("Informe o nome completo: ")
@@ -72,7 +72,7 @@ def criar_usuario(usuarios):
 
     usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
 
-    print("=== Usuário criado com sucesso! ===")
+    print(" Usuário criado com sucesso! ")
 
 
 def filtrar_usuario(cpf, usuarios):
@@ -85,10 +85,10 @@ def criar_conta(agencia, numero_conta, usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n=== Conta criada com sucesso! ===")
+        print("\n Conta criada com sucesso! ")
         return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
 
-    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+    print("\n Usuário não encontrado, fluxo de criação de conta encerrado! ")
 
 
 def listar_contas(contas):
